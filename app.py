39d2ddfc3a8aab27696892b36781d4670da36c71
TK7 <tktomo7260t@gmail,com>
2025-01-06 04:16:16 +0900

    404ページを追加し、エントリーページとプレイエリアのデータ取得を改善

--- app.py
+++ app.py
@@ -23,21 +23,39 @@ def test_data():
 
 @app.route("/entry/<roomid>/")
 def entry(roomid):
-    roomdata = {
-        "roomid": roomid,
-        "name": "データサイエンスプログラミングX",
-        "comment": "データサイエンスプログラミングXのエントリーページです。",
-    }
-    return render_template("/entry/page.html", roomdata=roomdata)
+    try:
+        db = dataset.connect('sqlite:///chat.db')
+        room_table = db['room']
+        roomdata = room_table.find_one(roomid=roomid)
+        db.executable.invalidate()
+        db.executable.engine.dispose()
+        db.close()
+        returnData = {
+            "roomid": roomid,
+            "name": roomdata["name"],
+            "comment": roomdata["comment"],
+        }
+        return render_template("/entry/page.html", roomdata=returnData)
+    except:
+        return "404 Not Found"
 
 @app.route('/entry/<roomid>/playarea/')
 def playarea(roomid):
-    roomdata = {
-        "roomid": roomid,
-        "name": "データサイエンスプログラミングX",
-        "comment": "データサイエンスプログラミングXのエントリーページです。",
-    }
-    return render_template("/entry/playarea/page.html", roomdata=roomdata)
+    try:
+        db = dataset.connect('sqlite:///chat.db')
+        room_table = db['room']
+        roomdata = room_table.find_one(roomid=roomid)
+        db.executable.invalidate()
+        db.executable.engine.dispose()
+        db.close()
+        returnData = {
+            "roomid": roomid,
+            "name": roomdata["name"],
+            "comment": roomdata["comment"],
+        }
+        return render_template("/entry/playarea/page.html", roomdata=returnData)
+    except:
+        return "404 Not Found"
 
 
 @app.route('/test/realtime/')
@@ -56,17 +74,34 @@ def get_roomdata(roomid):
     return jsonify(roomdata)
 
 @socketio.on('join')
-def on_join(roomid):
-    print(roomid)
+def on_join(joinData):
+    print("received id: " + joinData["roomid"])
+    print("received id: " + joinData["userid"])
     db = dataset.connect('sqlite:///chat.db')
-    room_table = db[roomid]
+    text_db = dataset.connect('sqlite:///textid.db')    
+    room_table = db[joinData["roomid"]]
     data = []
+    isLike = []
     for row in room_table:
         data.append(row)
+        if text_db[row["textid"]].find_one(userid=joinData["userid"]) != None:
+            isLike.append(True) 
+        else:
+            isLike.append(False)
+    
+    print(isLike)
+    
+    returnData = {
+        "data": data,
+        "isLike": isLike
+    }
+    text_db.executable.invalidate()
+    text_db.executable.engine.dispose()
+    text_db.close()
     db.executable.invalidate()
     db.executable.engine.dispose()
     db.close()
-    emit("message_history", data, broadcast=False)
+    emit("message_history", returnData, broadcast=False)
 
 @socketio.on('message')
 def handle_message(message, roomid):
@@ -78,9 +113,49 @@ def handle_message(message, roomid):
     db.executable.invalidate()
     db.executable.engine.dispose()
     db.close()
+    text_db = dataset.connect('sqlite:///textid.db')
+    text_table = text_db[message["textid"]]
+    text_table.insert(dict(userid=message["userid"]))
+    text_db.executable.invalidate()
+    text_db.executable.engine.dispose()
+    text_db.close()
     
     returnData = { "text": message["text"], "textid": message["textid"], "userid": message["userid"], "likes": 0, "isAnswerd": False }
     emit("new_message", returnData, broadcast=True)
+
+@socketio.on('like_plus')
+def handle_like_plus(data):
+    print('received textid: ' + data["textid"])
+    print('received roomid: ' + data["roomid"])
+    print('received userid: ' + data["userid"])
+    text_db = dataset.connect('sqlite:///textid.db')
+    text_table = text_db[data["textid"]]
+    #text_tableにtextidが存在するか確認
+    
+
+
+    if text_table.find_one(userid=data["userid"]) == None:
+        text_table.insert(dict(userid=data["userid"]))
+        db = dataset.connect('sqlite:///chat.db')
+        chat_table = db[data["roomid"]]
+        chatdata = chat_table.find_one(textid=data["textid"])
+        chatdata["likes"] += 1
+        chat_table.update(chatdata, ['textid'])
+        db.executable.invalidate()
+        db.executable.engine.dispose()
+        db.close()
+        text_db.executable.invalidate()
+        text_db.executable.engine.dispose()
+        text_db.close()
+        returnData = { 
+            "textid": data["textid"] ,
+            "index" : data["index"]
+        }
+        emit("like_plus_return", returnData, broadcast=True)
+    else:
+        pass
+
+    
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
--- dbtest.py
+++ dbtest.py
@@ -2,11 +2,8 @@ import dataset
 
 db = dataset.connect('sqlite:///chat.db')
 room_table = db['room']
-room_table.insert(dict(roomid="abc", name="データサイエンスプログラミングX", comment="データサイエンスプログラミングXのエントリーページです。"))
-room_table.insert(dict(roomid="efg", name="データサイエンスプログラミングY", comment="データサイエンスプログラミングYのエントリーページです。"))
+room_table.insert(dict(roomid="lll", name="データサイエンスプログラミング2", comment="データサイエンスプログラミングXのエントリーページです。"))
 
-chat_table = db['abc']
-chat_table.insert(dict(text="こんにちは", textid="1", userid="1", likes=0, isAnswerd=False))
 
 db.executable.invalidate()
 db.executable.engine.dispose()
